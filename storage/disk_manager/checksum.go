1106fc8013bf5e079bf6dc74093cd70a1bcb003e
ish4n10 <ishan.tripathi010@gmail.com>
2026-02-26 01:14:49 +0530

    update checksum

--- storage/disk_manager/checksum.go
+++ storage/disk_manager/checksum.go
@@ -4,7 +4,6 @@ import "encoding/binary"
 
 func ComputeChecksum(buf []byte) uint32 {
 	var checksum uint32
-	// use len(buf)-3 to never go out of bounds
 	for i := 0; i <= len(buf)-4; i += 4 {
 		checksum ^= binary.LittleEndian.Uint32(buf[i : i+4])
 	}
