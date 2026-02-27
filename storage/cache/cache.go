1f27014bcfb868be3ab61da23f8f1055bf160f80
ish4n10 <ishan.tripathi010@gmail.com>
2026-02-27 21:55:15 +0530

    add NewCache

--- storage/cache/cache.go
+++ storage/cache/cache.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 
 	diskmanager "github.com/ish4n10/miniaturedb/storage/disk_manager"
+	page "github.com/ish4n10/miniaturedb/storage/page"
 	ref "github.com/ish4n10/miniaturedb/storage/ref"
 )
 
@@ -19,4 +20,37 @@ type Cache struct {
 	bytesDirty uint64
 	dm         *diskmanager.DiskManager
 	mu         sync.Mutex
+}
+
+func (c *Cache) usagePercentage() float64 {
+	used := 0
+	for _, r := range c.refs {
+		if r.State != ref.RefStateDisk {
+			used++
+		}
+	}
+	return float64(used) / float64(c.maxPages)
+}
+
+func NewCache(dm *diskmanager.DiskManager, maxPages int) *Cache {
+	refs := make([]*ref.Ref, maxPages)
+
+	for i := range refs {
+		refs[i] = &ref.Ref{
+			State:    ref.RefStateDisk,
+			PageID:   0,
+			Page:     nil,
+			PinCount: 0,
+			LastUsed: 0,
+		}
+	}
+
+	return &Cache{refs: refs, maxPages: maxPages, byteInMem: 0, bytesDirty: 0, dm: dm}
+}
+
+func (c *Cache) evict() (*ref.Ref, error) {
+
+}
+func FetchPage(pageID uint32) (*page.Page, error) {
+
 }
