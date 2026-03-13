ef476374ce7d5b5ccb02666a245334baa400b8e9
ish4n10 <ishan.tripathi010@gmail.com>
2026-03-14 00:08:47 +0530

    todo: add parser

--- sql/parser/parser.go
+++ sql/parser/parser.go
@@ -6,3 +6,30 @@ type Parser struct {
 	tokens []lexer.Token
 	pos    int
 }
+
+func NewParser(tokens []lexer.Token) *Parser {
+	return &Parser{tokens: tokens, pos: 0}
+}
+
+// func (p *Parser) Parse() (interface{}, error) {
+
+// }
+
+func (p *Parser) current() lexer.Token {
+	if p.pos > len(p.tokens) {
+		return lexer.Token{Type: lexer.TOKEN_EOF, Literal: ""}
+	}
+	return p.tokens[p.pos]
+}
+
+func (p *Parser) advance() lexer.Token {
+	if p.pos+1 > len(p.tokens) {
+		return lexer.Token{Type: lexer.TOKEN_EOF, Literal: ""}
+	}
+	p.pos++
+	return p.current()
+}
+
+// func (p *Parser) expect(tt lexer.TokenType) (lexer.Token, error) {
+
+// }
