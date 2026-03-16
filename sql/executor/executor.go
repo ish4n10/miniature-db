84a65be40149d7ee50228bb718b9fe1fb0509acc
ish4n10 <ishan.tripathi010@gmail.com>
2026-03-16 19:25:49 +0530

    add delete

--- sql/executor/executor.go
+++ sql/executor/executor.go
@@ -1,6 +1,9 @@
 package executor
 
 import (
+	"fmt"
+
+	"github.com/ish4n10/miniaturedb/sql/lexer"
 	parser "github.com/ish4n10/miniaturedb/sql/parser"
 	db "github.com/ish4n10/miniaturedb/storage"
 )
@@ -29,8 +32,23 @@ func (e *Executor) executeDropTable(stmt *parser.DropTableStmt) (*Result, error)
 	return &Result{}, nil
 }
 
-func (e *Executor) executeInsertTable(stmt *parser.InsertStmt) (*Result, error) {
+func (e *Executor) executeInsert(stmt *parser.InsertStmt) (*Result, error) {
 	err := e.db.Put(stmt.TableName, []byte(stmt.Key), []byte(stmt.Value))
+	if err != nil {
+		return nil, err
+	}
+	return &Result{Affected: 1}, nil
+}
+
+func (e *Executor) executeDelete(stmt *parser.DeleteStmt) (*Result, error) {
+
+	if stmt.Where == nil {
+		return nil, fmt.Errorf("DELETE needs a WHERE")
+	}
+	if stmt.Where.Op != lexer.TOKEN_EQ {
+		return nil, fmt.Errorf("DELETE needs a =")
+	}
+	err := e.db.Delete(stmt.TableName, []byte(stmt.Where.Value))
 	if err != nil {
 		return nil, err
 	}
