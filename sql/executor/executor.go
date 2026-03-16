24a831c3190337a27c465a99f57324905d29848d
ish4n10 <ishan.tripathi010@gmail.com>
2026-03-16 19:18:12 +0530

    add createtableext

--- sql/executor/executor.go
+++ sql/executor/executor.go
@@ -1,7 +1,22 @@
 package executor
 
-import db "github.com/ish4n10/miniaturedb/storage"
+import (
+	parser "github.com/ish4n10/miniaturedb/sql/parser"
+	db "github.com/ish4n10/miniaturedb/storage"
+)
 
 func NewExecutor(db *db.DB) *Executor {
 	return &Executor{db: db}
+}
+
+func (e *Executor) Execute(stmt parser.Statement) (*Result, error) {
+
+}
+
+func (e *Executor) executeCreateTable(stmt *parser.CreateTableStmt) (*Result, error) {
+	err := e.db.CreateTable(stmt.TableName)
+	if err != nil {
+		return nil, err
+	}
+	return &Result{}, nil
 }
